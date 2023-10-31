83c5a19c094f5f28de7b0c922d589e9d511efe81
Miguel <miguel.garon@gmail.com>
2023-10-31 04:06:12 +0100

    changed window size

--- RiZzEngine/Globals.h
+++ RiZzEngine/Globals.h
@@ -1,8 +1,8 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
-#define WINDOW_HEIGHT 1000
-#define WINDOW_WIDTH 1000
+#define WINDOW_HEIGHT 700
+#define WINDOW_WIDTH 1200
 #define WINDOW_TITLE "RizzEngine"
 
 #define CAMERA_INIT_POS glm::vec3(0.0f, 0.0f, 2.0f)
